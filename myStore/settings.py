9f2e69829936241d4cd1441a93578bc8fe4ba9de
Akinwale Akinseye <dewhalles@gmail.com>
2024-08-23 11:58:38 -0400

    fixed typo in settings file

--- myStore/settings.py
+++ myStore/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "user.apps.UserConfig",
-    "product.apps.ProductConfig"
+    "product.apps.ProductConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
